f22d86df43b34d2687228e5b2411cbaf6640557e
237legendmola <ebongvictory222@gmail.com>
2025-08-02 06:49:17 -0700

    push code

---
+++ java.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+  const media = document.querySelector(".media-list");
+  const media = document.querySelector(".hamburgar-icon");   
+  media.classList.toggle("open");
+  icon.classList.toggle("open");
+}
